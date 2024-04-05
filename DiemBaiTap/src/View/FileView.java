262fb75272e990fd8f3d8313caeb42ab5933a1b5
PhamTuanHuwng <152506994+PhamTuanHuwng@users.noreply.github.com>
2024-04-05 08:08:53 +0700

    Add files via upload

---
+++ DiemBaiTap/src/View/FileView.java
@@ -0,0 +1,16 @@
+package View;
+
+import javax.swing.JTextArea;
+
+public class FileView {
+    private JTextArea textArea;
+
+    public FileView() {
+        textArea = new JTextArea(5, 30);
+        textArea.setEditable(false);
+    }
+
+    public JTextArea getTextArea() {
+        return textArea;
+    }
+}
